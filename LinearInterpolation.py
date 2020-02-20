0b75fbcd88c4035403b48bb7bef911678a93e153
Yusuf Dede <vertical99@hotmail.com>
2020-02-20 05:40:34 +0300

    division by zero fix

--- LinearInterpolation.py
+++ LinearInterpolation.py
@@ -1,7 +1,5 @@
 def start(y0, x, x0, x1, y1):
-    print("y0", y0,"x", x, "x0", x0, "x1", x1, "y1",y1)
-    y = (x - x0) * (y1-y0) / (x1-x0)
-    return y0 + y
-
-
-print(start(1355, 6, 4, 7, 1680))
+    try:
+        return y0 + (x - x0) * (y1-y0) / (x1-x0)
+    except ZeroDivisionError:
+        return 'division by zero!!!'    
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, escape, request, render_template
+from LinearInterpolation import start
 
 app = Flask(__name__)   
 
@@ -13,7 +14,21 @@ def main():
 
 @app.route('/calc', methods=['POST', 'GET'])
 def calc():
+    
+    if request.method == 'POST':
+        y0 = request.form.get('y0')
+        x = request.form.get('x')
+        x0 = request.form.get('x0')
+        y1 = request.form.get('y1')
+        x1 = request.form.get('x1')
 
+        # start(y0, x, x0, x1, y1):
+        print(start(int(y0), int(x), int(x0), int(x1), int(y1) ))
+        return render_template('index.html', result=start(int(y0), int(x), int(x0), int(x1), int(y1)))
+    else:
+        return render_template('index.html', result="değerleri gir!")
+    
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
